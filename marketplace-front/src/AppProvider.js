8384b5a7ad7b5942bda29499225fc82f677ae9eb
unknown <rachidboss027@gmail.com>
2025-07-24 21:02:59 -0400

    chore: remove old frontend for fresh rebuild

--- marketplace-front/src/AppProvider.js
+++
@@ -1,2 +0,0 @@
-//Proveedor del Context API para el estado global
-// Proveedor de estado global utilizando Context API
--- marketplace-front/src/services/api.js
+++
@@ -1,9 +0,0 @@
-// ✅ src/services/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: "https://mskplace-caro-rachid-2024.onrender.com/api",
-  withCredentials: true
-});
-
-export default api;
